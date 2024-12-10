401afb3cd5dc1c1664aa919505d716f006f6e46b
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2024-12-10 14:03:54 +0000

    Add exception and display on Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }       
 }
@@ -65,13 +66,6 @@ function displayErrors(data) {
 }
 
 
-
-
-
-
-
-
-
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
@@ -82,14 +76,28 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
+
 
 function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your Key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
+
+function displayException(data) {
+    let heading = `An Exception occured`;
+    results = `<div>The API return status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
