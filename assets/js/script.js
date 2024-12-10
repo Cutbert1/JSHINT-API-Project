795ff42761aad399cd6a71b6f6e791c67ae2a45f
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2024-12-10 12:32:43 +0000

    Add functions to convert response to json and display results

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,48 @@ document.getElementById("submit").addEventListener("click", e => postForm(e));
 async function postForm(e) {
     const form = new FormData(document.getElementById("checksform"));
 
-    const response = await fetch("API_URL", {
+    const response = await fetch(API_URL, {
         method: "POST",
         headers: {
             "Authorization": API_KEY,
         },
         body: form,
-    })    
-        
+    });
+    
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }       
 }
+
+function displayErrors(data) {
+
+    let heading = `JSHint Result for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no-errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error-count">${data.total_errors}</span></div>`
+        for (let error of data.error_list) {
+            results += `<div>At line<span class="line">${error.line}</span>,`;
+            results += `column<span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+
+}
+
+
+
+
+
 
 
 
